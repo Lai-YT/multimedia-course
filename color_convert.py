1eb2309279027628b61720d8905904972d8a32df
Lai-YT <381xvmvbib@gmail.com>
2021-03-06 09:58:54 +0800

    add comment

--- color_convert.py
+++ color_convert.py
@@ -14,26 +14,38 @@ cv2.imwrite('pic/ycrcb.jpg', ycrcb_img)
 hsv_img = cv2.cvtColor(origin_img, cv2.COLOR_BGR2HSV)
 cv2.imwrite('pic/hsv.jpg', hsv_img)
 
+# -----techs-----
+# An image is a 3-dimensinal numpy array of the form [pixels in height, pixels in width, number of channels],
+# a color image has 3 channels, 0-blue, 1-green, 2-red; a gray image has only one channel.
+
 # -----bgr-gray-----
+# [:, :, n] means to keep the height and width, but channel n only. So is a gray image.
 
 blue_gray_img = cv2.imwrite('pic/blue_gray.jpg', origin_img[:, :, 0])
-ggreen_gray_img = cv2.imwrite('pic/green_gray.jpg', origin_img[:, :, 1])
+green_gray_img = cv2.imwrite('pic/green_gray.jpg', origin_img[:, :, 1])
 gred_gray_img = cv2.imwrite('pic/red_gray.jpg', origin_img[:, :, 2])
 
 # -----bgr-color-----
+# If wan the blue-color-only image, let [:, :, 1] and [:, :, 2] be all zeros.
+# Since still has 3 channels, it's a color image.
 
+# create a template image with three channels all zero: black image
 height = origin_img.shape[0]
-weight = origin_img.shape[1]
-template_img = np.zeros((height, weight, 3), np.uint8)
+width = origin_img.shape[1]
+template_img = np.zeros((height, width, 3), np.uint8)
+cv2.imwrite('pic/template.jpg', template_img)
 
 blue_color_img = template_img.copy()
+# get color blue
 blue_color_img[:, :, 0] = origin_img[:, :, 0]
 cv2.imwrite('pic/blue_color.jpg', blue_color_img)
 
 green_color_img = template_img.copy()
+# get color green
 green_color_img[:, :, 1] = origin_img[:, :, 1]
 cv2.imwrite('pic/green_color.jpg', green_color_img)
 
 red_color_img = template_img.copy()
+# get color red
 red_color_img[:, :, 2] = origin_img[:, :, 2]
 cv2.imwrite('pic/red_color.jpg', red_color_img)
