3bb876bfd00f92fd5d099a334aaca43fdd935d48
Lai-YT <381xvmvbib@gmail.com>
2021-03-09 11:07:18 +0800

    add shape and text

--- color_convert.py
+++ color_convert.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 
 origin_img = cv2.imread('pic/donut.jpg')
+height = origin_img.shape[0] # 823
+width = origin_img.shape[1] # 936
 
 # ----------
 
@@ -30,8 +32,6 @@ gred_gray_img = cv2.imwrite('pic/red_gray.jpg', origin_img[:, :, 2])
 # Since still has 3 channels, it's a color image.
 
 # create a template image with three channels all zero: black image
-height = origin_img.shape[0]
-width = origin_img.shape[1]
 template_img = np.zeros((height, width, 3), np.uint8)
 cv2.imwrite('pic/template.jpg', template_img)
 
@@ -49,3 +49,27 @@ red_color_img = template_img.copy()
 # get color red
 red_color_img[:, :, 2] = origin_img[:, :, 2]
 cv2.imwrite('pic/red_color.jpg', red_color_img)
+
+# -----shape-drawing-----
+
+draw_img = origin_img
+
+# (target_img, left-start cord., right-end cord., color, border-thickness)
+cv2.line(draw_img, (height//2, 10), (10, width//2), (50, 150, 200), 5)
+
+# (target_img, upper-left cord., lower-right cord., color, border-thickness)
+cv2.rectangle(draw_img, (80, 550), (350, 800), (0, 200, 200), 5)
+
+# (target_img, center cord., radius, color, border-thickness)
+cv2.circle(draw_img, (width//2 + 290, height//2 + 10), 350, (150, 0, 250), 5)
+cv2.circle(draw_img, (width//2 + 290, height//2 + 10), 100, (200, 0, 200), 5)
+
+# note that border-thickness = -1 means to fill up the shape
+
+# -----text-----
+
+text = 'donuts!'
+font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+# (target_img, message, start cord., font-style, font-size, color, thickness)
+cv2.putText(draw_img, text, (120, 200), font, 10, (255, 100, 200), 5)
+cv2.imwrite('pic/draw.jpg', draw_img)
