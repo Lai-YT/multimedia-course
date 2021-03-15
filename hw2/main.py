32d01579d3248d4c9e231ea881622d1943a1e625
Lai-YT <381xvmvbib@gmail.com>
2021-03-15 22:02:38 +0800

    add comment about using dilate and erode

--- hw2/main.py
+++ hw2/main.py
@@ -11,7 +11,11 @@ ret, circle_bin = cv2.threshold(circle_gray, 127, 255, cv2.THRESH_BINARY)
 cv2.imwrite('pic/circle_bin.png', circle_bin)
 
 # using close operation to get rid of small black spot.
-# same as dilate first, and than erode
+# same as dilate first, and than erode, which are the two following lines:
+
+# circle_dilate = cv2.dilate(circle_bin, np.ones((5, 6)), iterations=5)
+# circle_unspot = cv2.erode(circle_dilate, np.ones((5, 6)), iterations=5)
+
 circle_unspot = cv2.morphologyEx(circle_bin, cv2.MORPH_CLOSE, np.ones((5, 6)), iterations=5)
 cv2.imwrite('pic/circle_unspot.png', circle_unspot)
 
@@ -25,6 +29,11 @@ ret, man_bin = cv2.threshold(man_gray, 127, 255, cv2.THRESH_BINARY)
 cv2.imwrite('pic/man_bin.png', man_bin)
 
 # using gradient to get the contour line.
-# same as dilate - erode
+# same as dilate - erode, which are the three following lines:
+
+# man_dilate = cv2.dilate(man_bin, np.ones((3, 3)), iterations=2)
+# man_erode = cv2.erode(man_bin, np.ones((3, 3)), iterations=2)
+# man_border = man_dilate - man_erode
+
 man_border = cv2.morphologyEx(man_bin, cv2.MORPH_GRADIENT, np.ones((3, 3)), iterations=2)
 cv2.imwrite('pic/man_border.png', man_border)
