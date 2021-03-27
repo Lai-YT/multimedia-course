e6e8110c04e9c8fc0f224543b89b7bff477b1862
Lai-YT <381xvmvbib@gmail.com>
2021-03-27 23:29:53 +0800

    one line missed

--- hw3/floor.py
+++ hw3/floor.py
@@ -11,14 +11,18 @@ def get_floor() -> None:
     gray = cv2.cvtColor(floor, cv2.COLOR_BGR2GRAY)
     cv2.imwrite('floor_pic/gray.jpg', gray)
 
-    _, bin = cv2.threshold(gray, 95, 255, cv2.THRESH_BINARY)
-    cv2.imwrite('floor_pic/bin.jpg', bin)
+    _, bin = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    # cv2.imwrite('floor_pic/bin.jpg', bin)
+
+    # cv2.morphologyEx(bin, cv2.MORPH_OPEN, np.ones((3, 3)), iterations=5)
 
     blur = cv2.GaussianBlur(bin, (3, 3), 0)
     cv2.imwrite('floor_pic/blur.jpg', blur)
 
-    _, bin = cv2.threshold(blur, 190, 255, cv2.THRESH_BINARY)
+    _, bin = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY)
+
     cv2.imwrite('floor_pic/bin.jpg', bin)
+
 
     canny = cv2.Canny(bin, 50, 150)
     cv2.imwrite('floor_pic/canny.jpg', canny)
@@ -35,25 +39,6 @@ def get_floor() -> None:
         x2, y2 = int(x0 - 1000 * (-b)), int(y0 - 1000 * (a))
         # (target_img, left-start coord., right-end coord., color, [border-thickness, [line-type]])
         cv2.line(with_line, (x1, y1), (x2, y2), ct.red, 1, cv2.LINE_AA)
-
-    # _, bin = cv2.threshold(blur, 130, 255, cv2.THRESH_BINARY)
-    # cv2.imwrite('floor_pic/bin.jpg', bin)
-    #
-    # canny = cv2.Canny(bin, 50, 150)
-    # cv2.imwrite('floor_pic/canny.jpg', canny)
-    #
-    # lines = cv2.HoughLines(canny, 1, np.pi/4, 250)
-    #
-    # for line in lines:
-    #     rho, theta = line[0]
-    #     a, b = np.cos(theta), np.sin(theta)
-    #     x0, y0 = a * rho, b * rho
-    #     x1, y1 = int(x0 + 1000 * (-b)), int(y0 + 1000 * (a))
-    #     x2, y2 = int(x0 - 1000 * (-b)), int(y0 - 1000 * (a))
-    #     # (target_img, left-start coord., right-end coord., color, [border-thickness, [line-type]])
-    #     cv2.line(with_line, (x1, y1), (x2, y2), ct.red, 1, cv2.LINE_AA)
-
-    cv2.imwrite('floor_pic/line.jpg', with_line)
 
     return #  end get_floor
 
