4b3ee687c93f6eaece4c001b0bf970a714a030c7
Lai-YT <381xvmvbib@gmail.com>
2021-03-28 14:29:39 +0800

    floor line get

--- hw3/floor.py
+++ hw3/floor.py
@@ -12,33 +12,35 @@ def get_floor() -> None:
     cv2.imwrite('floor_pic/gray.jpg', gray)
 
     _, bin = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
-    # cv2.imwrite('floor_pic/bin.jpg', bin)
-
-    # cv2.morphologyEx(bin, cv2.MORPH_OPEN, np.ones((3, 3)), iterations=5)
 
     blur = cv2.GaussianBlur(bin, (3, 3), 0)
     cv2.imwrite('floor_pic/blur.jpg', blur)
 
-    _, bin = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY)
-
+    # upper left area [:200, 200:] is a bit special, so use another threshold
+    _, bin = cv2.threshold(blur, 160, 255, cv2.THRESH_BINARY)
+    _, bin2 = cv2.threshold(blur, 240, 255, cv2.THRESH_BINARY)
+    # composite
+    bin[:200, 200:] = bin2[:200, 200:]
     cv2.imwrite('floor_pic/bin.jpg', bin)
-
 
     canny = cv2.Canny(bin, 50, 150)
     cv2.imwrite('floor_pic/canny.jpg', canny)
 
-    lines = cv2.HoughLines(canny, 1, np.pi/4, 250)
 
     with_line = cv2.imread('floor_pic/resize.jpg')
 
+    lines = cv2.HoughLines(canny, 1, np.pi/4, 250)
     for line in lines:
         rho, theta = line[0]
         a, b = np.cos(theta), np.sin(theta)
         x0, y0 = a * rho, b * rho
         x1, y1 = int(x0 + 1000 * (-b)), int(y0 + 1000 * (a))
         x2, y2 = int(x0 - 1000 * (-b)), int(y0 - 1000 * (a))
+
         # (target_img, left-start coord., right-end coord., color, [border-thickness, [line-type]])
         cv2.line(with_line, (x1, y1), (x2, y2), ct.red, 1, cv2.LINE_AA)
+
+    cv2.imwrite('floor_pic/line.jpg', with_line)
 
     return #  end get_floor
 
